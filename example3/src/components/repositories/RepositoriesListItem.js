c3902c7a9c6bcae154bb01a128767e1bf3681e91
Willian Rios <williangrios@yahoo.com.br>
2024-05-23 13:14:45 -0300

    feat: new feature

--- example3/src/components/repositories/RepositoriesListItem.js
+++ example3/src/components/repositories/RepositoriesListItem.js
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom';
-import FileIcon from '../tree/FileIcon';
-import RepositoriesSummary from './RepositoriesSummary';
+import { Link } from 'react-router-dom'
+import FileIcon from '../tree/FileIcon'
+import RepositoriesSummary from './RepositoriesSummary'
+import { MarkGithubIcon } from '@primer/octicons-react'
 
 function RepositoriesListItem({ repository }) {
-  const { full_name, language, description, owner, name } = repository;
+  const { full_name, language, description, owner, name, html_url } = repository
 
   return (
     <div className="py-3 border-b flex">
@@ -15,8 +16,13 @@ function RepositoriesListItem({ repository }) {
         <p className="text-gray-500 italic py-1">{description}</p>
         <RepositoriesSummary repository={repository} />
       </div>
+      <div className="grow flex items-center justify-end pr-2">
+        <a href={repository.html_url} aria-label="github repository">
+          <MarkGithubIcon />
+        </a>
+      </div>
     </div>
-  );
+  )
 }
 
-export default RepositoriesListItem;
+export default RepositoriesListItem
--- example3/src/routes/HomeRoute.js
+++ example3/src/routes/HomeRoute.js
@@ -1,12 +1,14 @@
-import Hero from '../components/Hero';
-import RepositoriesTable from '../components/repositories/RepositoriesTable';
-import useRepositories from '../hooks/useRepositories';
+import Hero from '../components/Hero'
+import RepositoriesTable from '../components/repositories/RepositoriesTable'
+import useRepositories from '../hooks/useRepositories'
 
 function HomeRoute() {
-  const { data: jsRepos } = useRepositories('stars:>10000 language:javascript');
-  const { data: tsRepos } = useRepositories('stars:>10000 language:typescript');
-  const { data: rustRepos } = useRepositories('stars:>10000 language:rust');
-  const { data: goRepos } = useRepositories('stars:>10000 language:go');
+  const { data: jsRepos } = useRepositories('stars:>10000 language:javascript')
+  const { data: tsRepos } = useRepositories('stars:>10000 language:typescript')
+  const { data: rustRepos } = useRepositories('stars:>10000 language:rust')
+  const { data: goRepos } = useRepositories('stars:>10000 language:go')
+  const { data: javaRepos } = useRepositories('stars:>10000 language:java')
+  const { data: pythonRepos } = useRepositories('stars:>10000 language:python')
 
   return (
     <div>
@@ -22,9 +24,14 @@ function HomeRoute() {
         />
         <RepositoriesTable label="Most Popular Rust" repositories={rustRepos} />
         <RepositoriesTable label="Most Popular Go" repositories={goRepos} />
+        <RepositoriesTable label="Most Popular Java" repositories={javaRepos} />
+        <RepositoriesTable
+          label="Most Popular Python"
+          repositories={pythonRepos}
+        />
       </div>
     </div>
-  );
+  )
 }
 
-export default HomeRoute;
+export default HomeRoute
